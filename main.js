4ab4902168a45d05e1ba66ea25f88a5dc2b22b24
Misbah Jameel <162004635+Misbah-jameel@users.noreply.github.com>
2024-06-26 22:35:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+"use strict";
+// // NESTED ARRAY
+let array1 = ["MISBAH", "MARIYA", ["MADIHA", "ZAINAB", "MARIYAM"]];
+console.log("ARRAY", array1);
+console.log("index 0:", array1[0]);
+console.log("index 1:", array1[1]);
+console.log("index 2:", array1[2]);
+console.log("nested array index 0:", array1[2][0]);
+console.log("nested array index 1:", array1[2][1]);
+console.log("nested array index 2:", array1[2][2]);
+console.log("=====================================================");
+let array = ["MISBAH", "MARIYA", ["MADIHA", "ZAINAB", "MARIYAM"]];
+let nestedArray = array[2];
+console.log("Nested array index 0:", nestedArray[0]);
+console.log("Nested array index 1:", nestedArray[1]);
+console.log("Nested array index 2:", nestedArray[2]);
